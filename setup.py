8fa5dda3bc82a1eabfd72b822174a1aa55dbbdb8
Evgeny Bogodukhov <boevgeny@gmail.com>
2016-09-29 22:36:11 +0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except EnvironmentError:
 
 setup(
     name="evc",
-    version="0.1",
+    version="0.1.1",
     description=DESCRIPTION,
     long_description=long_description,
     license="MIT License",
